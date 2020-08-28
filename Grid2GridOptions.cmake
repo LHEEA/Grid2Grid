b9cca8e8df84cf339aee1615daecbbfa6ffd15dd
Young-Myung Choi <youngmyung.choi@bureauveritas.com>
2020-08-28 15:56:19 +0200

    Fix Bug and Improve ReadMe.
    Signed-off-by: Young-Myung Choi <youngmyung.choi@bureauveritas.com>

---
+++ Grid2GridOptions.cmake
@@ -0,0 +1,16 @@
+###-------------------------------------------------------------------------###
+###                                                                         ###
+###             Compilation variables for Grid2Grid Project                 ###
+###                                                                         ###
+###-------------------------------------------------------------------------###
+
+###... Fortran Compiler
+set(CMAKE_Fortran_COMPILER gfortran)
+# set(CMAKE_Fortran_COMPILER ifort)
+
+###... FFTW3 Library Path
+# set(FFTW3_LIB_PATH  HOME/fftw/fftw-3.3.8/lib)
+
+###... HDF5 Library Path
+# set(HDF5_LIB_PATH  $HOME/hdf5)
+# set(HDF_LIBRARY "ON")
